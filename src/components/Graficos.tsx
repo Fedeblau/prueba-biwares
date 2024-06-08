67f93dd7bd751f1ee8ce88df8886b41bf732ccbd
Fedeblau <153089163+Fedeblau@users.noreply.github.com>
2024-06-08 20:27:18 -0300

    charts

--- src/components/Graficos.tsx
+++ src/components/Graficos.tsx
@@ -35,6 +35,14 @@ type Movie = {
     Western: string;
 };
 
+type User = {
+    user_id: number;
+    age: number;
+    gender: string;
+    occupation: string;
+    zip_code: string;
+};
+
 type GenreYearRating = {
     [genre: string]: {
         [year: number]: {
@@ -58,31 +66,31 @@ interface GraficosProps {
 }
 
 const Graficos: React.FC<GraficosProps> = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [histograma, setHistograma] = useState([]);
 
-    const [genero, setGenero] = useState([])
+    const genres = ['Action', 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'];
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const [response1, response2] = await Promise.all([
+                const [response1, response2, response3] = await Promise.all([
                     fetch('https://script.google.com/macros/s/AKfycbzkTqrecKUFNvqUtK279UB01r0oNYaSOzSj8QthrooK_-Sj0LLomQ7KbJZMhH3hGB6R/exec'),
                     fetch('https://script.google.com/macros/s/AKfycbynvRUOEdQV-cNWj3NBh4az3DJpNHaK9tbNE-ppHG50DifyBNxcAvKqEpaln1mdWQ2f/exec'),
-
+                    fetch('https://script.google.com/macros/s/AKfycbyzFbdkHoPBORZcFBX1Ob-cp0W6qHa3hUWBXrBg35sceGmpjUZlRjMwt44tj3z_D4Dtgw/exec')
                 ]);
 
                 const peliculas = await response1.json() as Movie[];
                 const ratings = await response2.json() as Rating[];
+                const users = await response3.json() as User[];
 
                 const moviesDict: { [key: string]: Movie } = {};
                 peliculas.forEach(movie => {
                     moviesDict[movie.id] = movie;
                 });
 
-                // Inicializar un diccionario para almacenar las sumas de las puntuaciones y los conteos por año y género
                 const genreYearRating: GenreYearRating = {};
 
-                // Procesar las puntuaciones
                 ratings.forEach(rating => {
                     const movie = moviesDict[rating.movie_id];
                     if (movie) {
@@ -104,7 +112,6 @@ const Graficos: React.FC<GraficosProps> = () => {
                     }
                 });
 
-                // Calcular el promedio de puntuación por género y año
                 const genreYearAvgRating: GenreYearAvgRating[] = [];
 
                 Object.keys(genreYearRating).forEach(genre => {
@@ -118,28 +125,103 @@ const Graficos: React.FC<GraficosProps> = () => {
                         });
                     });
                 });
-                setData(genreYearAvgRating)
+
+                function groupRatingsByGenre(ratings, movies) {
+                    const genreRatings = {};
+
+                    movies.forEach(movie => {
+                        Object.keys(movie).forEach(key => {
+                            if (key !== "id" && key !== "Name" && key !== "Release Date" && key !== "IMDB URL" && movie[key] === "1") {
+                                if (!genreRatings[key]) {
+                                    genreRatings[key] = [];
+                                }
+
+                                const movieId = parseInt(movie.id);
+                                const movieRatings = ratings.filter(rating => rating.movie_id === movieId);
+                                movieRatings.forEach(rating => {
+                                    genreRatings[key].push(rating.rating);
+                                });
+                            }
+                        });
+                    });
+
+                    return genreRatings;
+                }
+
+                const genreRatingCounts = {};
+
+                genres.forEach(genre => {
+                    genreRatingCounts[genre] = {
+                        1: { count: 0, "0-18": 0, "19-25": 0, "26-45": 0, "+46": 0, users: new Set(), movies: new Set() },
+                        2: { count: 0, "0-18": 0, "19-25": 0, "26-45": 0, "+46": 0, users: new Set(), movies: new Set() },
+                        3: { count: 0, "0-18": 0, "19-25": 0, "26-45": 0, "+46": 0, users: new Set(), movies: new Set() },
+                        4: { count: 0, "0-18": 0, "19-25": 0, "26-45": 0, "+46": 0, users: new Set(), movies: new Set() },
+                        5: { count: 0, "0-18": 0, "19-25": 0, "26-45": 0, "+46": 0, users: new Set(), movies: new Set() }
+                    };
+                });
+
+                ratings.forEach(rating => {
+                    const { rating: userRating, user_id, movie_id } = rating;
+                    const movie = moviesDict[movie_id];
+                    if (movie) {
+                        genres.forEach(genre => {
+                            if (movie[genre] === "1") {
+                                const genreData = genreRatingCounts[genre][userRating];
+                                genreData.count++;
+                                genreData.users.add(user_id);
+                                genreData.movies.add(movie_id);
+                            }
+                        });
+                    }
+                });
+
+            
+            
+              
+
+                
+                const userAges = users.map(e=>1998-parseInt(e['year of birth']))
+               
+
+                Object.keys(genreRatingCounts).forEach(genre => {
+                    Object.keys(genreRatingCounts[genre]).forEach(rating => {
+                        const usersSet = genreRatingCounts[genre][rating].users;
+                        usersSet.forEach(user_id => {
+                            const age = userAges[user_id];
+                            
+                            if (age*1 <= 18) {
+                                genreRatingCounts[genre][rating]["0-18"]++;
+                            } else if (age*1 <= 25) {
+                                console.log("entro aca")
+                                genreRatingCounts[genre][rating]["19-25"]++;
+                            } else if (age*1 <= 45) {
+                                genreRatingCounts[genre][rating]["26-45"]++;
+                            } else {
+                                genreRatingCounts[genre][rating]["+46"]++;
+                            }
+                        });
+                        genreRatingCounts[genre][rating].users = Array.from(usersSet);
+                        genreRatingCounts[genre][rating].movies = Array.from(genreRatingCounts[genre][rating].movies);
+                    });
+                });
+
+                console.log("genreRatingCounts", genreRatingCounts);
+
+                setHistograma(genreRatingCounts);
+
+                setData(genreYearAvgRating);
+            } catch (error) {
+                console.error("se rompio todo");
             }
-            catch(error) {
-                console.error("se rompio todo")
-            }
-        }
-            fetchData()
-        }, []);
+        };
+        fetchData();
+    }, []);
 
-
-
-    // Convertir la lista de películas a un diccionario para acceso rápido
-
-
-    console.log("Promedios de puntuación por género y año:", data);
+    console.log("histograma!!!", histograma);
 
     return (
         <>
-            {
-                data &&
-                <BarChart className="aspect-[6/3] max-w-[75vw] text-black" data={data} />
-            }
+            <BarChart className="aspect-[6/3] max-w-[75vw] text-black" data={data} histograma={histograma} />
         </>
     );
 };
--- src/components/puntuacion/bar-chart.tsx
+++ src/components/puntuacion/bar-chart.tsx
@@ -1,71 +1,147 @@
 
-import {ResponsiveBar} from '@nivo/bar';
+import { ResponsiveBar } from '@nivo/bar';
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 
 export const BarChart = (props) => {
-    const data = props.data;
-  
-    const getYears = (data) => {
-        // Crear un conjunto para almacenar los valores únicos
-        let valoresUnicos = new Set();
-    
-        // Iterar sobre los objetos en los datos
-        data.forEach((dato) => {
-            // Agregar el valor de la clave "year" al conjunto
-            valoresUnicos.add(dato.year);
-        });
-    
-        // Convertir el conjunto de valores únicos a un array y devolverlo
-        return Array.from(valoresUnicos);
+    const { data, histograma } = props
+    const [genre, setGenero] = useState(null)
+    const [graf, setGraf] = useState(data)
+    const [histoGraf, setHistoGraf] = useState(data)
+
+    const genres = ['Action', 'Adventure', 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'];
+
+    function transformarObjetoAArray(obj) {
+        return Object.entries(obj).map(([clave, valor]) => ({
+            prop: clave,
+            ...valor
+        }));
     }
 
-    const years = getYears(data)
 
-    console.log("years ", years)
+    useEffect(() => {
+        if (genre) {
+            setGraf(data.filter(e => e.genre === genre))
+            console.log("genre", genre)
+            const arr = transformarObjetoAArray(histograma[genre])
+            console.log(transformarObjetoAArray(histograma[genre]))
+            setHistoGraf(arr)
+        }
+    }, [genre])
 
-  
+
+
+    console.log("HISTOGRAF!!!! ", histoGraf)
+
+
+
     return (
-      <div {...props}>
-        <ResponsiveBar
-          data={data}
-          keys={["average_rating"]}
-          indexBy="year"
-          margin={{ top: 0, right: 0, bottom: 40, left: 40 }}
-          padding={0.3}
-          colors={["#2563eb"]}
-          axisBottom={{
-            tickSize: 0,
-            tickPadding: 16,
-          }}
-          axisLeft={{
-            tickSize: 0,
-            tickValues: 4,
-            tickPadding: 16,
-          }}
-          gridYValues={4}
-          theme={{
-            tooltip: {
-              chip: {
-                borderRadius: "9999px",
-              },
-              container: {
-                fontSize: "12px",
-                textTransform: "capitalize",
-                borderRadius: "6px",
-              },
-            },
-            grid: {
-              line: {
-                stroke: "#f3f4f6",
-              },
-            },
-          }}
-          tooltipLabel={({ id }) => `${id}`}
-          enableLabel={false}
-          role="application"
-          ariaLabel="peliculas por rating"
-        />
-      </div>
+        <>
+            <header className="mt-20 text-white">
+                {
+                    genres.map(g => <Button key={g} variant={"borde"} onClick={() => setGenero(g)}>{g}</Button>)
+                }
+            </header>
+
+            <div className='grid grid-cols-2'>
+
+                <div>
+
+                
+                        <h2 className='text-2xl my-10 font text-white'>{genre || "Selecciona un género"}</h2>
+                    
+                    <div {...props}>
+                        <ResponsiveBar
+
+                            data={graf}
+                            keys={["average_rating"]}
+                            indexBy="year"
+                            margin={{ top: 0, right: 0, bottom: 40, left: 40 }}
+                            padding={0.3}
+                            colors={["#2563eb"]}
+                            axisBottom={{
+                                tickSize: 0,
+                                tickPadding: 16,
+                            }}
+                            axisLeft={{
+                                tickSize: 0,
+                                tickValues: 5,
+                                tickPadding: 16,
+                            }}
+                            gridYValues={5}
+                            theme={{
+                                tooltip: {
+                                    chip: {
+                                        borderRadius: "9999px",
+                                    },
+                                    container: {
+                                        fontSize: "12px",
+                                        textTransform: "capitalize",
+                                        borderRadius: "6px",
+                                    },
+                                },
+                                grid: {
+                                    line: {
+                                        stroke: "#f3f4f6",
+                                    },
+                                },
+                            }}
+                            tooltipLabel={({ id }) => `${id}`}
+                            enableLabel={false}
+                            role="application"
+                            ariaLabel="peliculas por rating"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <h2 className='text-2xl my-10 font text-white'>Histograma</h2>
+                    <div {...props}>
+                        <ResponsiveBar
+                            data={histoGraf}
+                            keys={["0-18","19-25","26-45","+46"]}
+                            indexBy="prop"
+                            margin={{ top: 0, right: 0, bottom: 40, left: 40 }}
+                            padding={0.3}
+                            
+                            axisBottom={{
+                                tickSize: 0,
+                                tickPadding: 16,
+                            }}
+                            axisLeft={{
+                                tickSize: 0,
+                                tickValues: 5,
+                                tickPadding: 16,
+                            }}
+                            gridYValues={5}
+                            theme={{
+                                tooltip: {
+                                    chip: {
+                                        borderRadius: "9999px",
+                                    },
+                                    container: {
+                                        fontSize: "12px",
+                                        textTransform: "capitalize",
+                                        borderRadius: "6px",
+                                    },
+                                },
+                                grid: {
+                                    line: {
+                                        stroke: "#f3f4f6",
+                                    },
+                                },
+                            }}
+                            tooltipLabel={({ id }) => `${id}`}
+                            enableLabel={false}
+                            role="application"
+                            ariaLabel="peliculas por rating"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+
+
     );
-  }
-  
+}
+
